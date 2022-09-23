cc57abe15d8bfe62b61e7d32f949dc665db58a38
Maxim Shepelyakvoski <maxim.shepelyakovski@yahoo.com>
2022-09-23 14:09:06 +0300

    mapping

--- src/main/java/com/softkit/mapper/UserMapper.java
+++ src/main/java/com/softkit/mapper/UserMapper.java
@@ -4,9 +4,18 @@ import com.softkit.dto.UserDataDTO;
 import com.softkit.dto.UserResponseDTO;
 import com.softkit.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+
+    @Mapping(target ="username",source = "username" )
+    @Mapping(target ="email",source = "email" )
+    @Mapping(target ="password",source = "password" )
+    @Mapping(target ="firstName",source = "firstName" )
+    @Mapping(target ="lastName",source = "lastName" )
+    @Mapping(target ="birthday",source = "birthday" )
+    @Mapping(target ="roles",source = "roles" )
     User mapUserDataToUser(UserDataDTO e);
 
     UserResponseDTO mapUserToResponse(User e);
