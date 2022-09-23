10ebdd180a4e8b9f0093a21732e1d742273761b9
Maxim Shepelyakvoski <maxim.shepelyakovski@yahoo.com>
2022-09-23 14:07:10 +0300

    adding new fields

--- src/main/java/com/softkit/model/User.java
+++ src/main/java/com/softkit/model/User.java
@@ -1,18 +1,22 @@
 package com.softkit.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
 
 @Entity(name = "Users")
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements Serializable {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,13 +25,40 @@ public class User {
     @Size(min = 4, max = 255, message = "Minimum username length: 4 characters")
     @Column(unique = true, nullable = false)
     private String username;
-    
-    @Column(unique = true, nullable = false)
-    private String email;
-    
+
     @Size(min = 8, message = "Minimum password length: 8 characters")
     private String password;
 
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "UpdateEmail",nullable = true)
+    private String updateEmail;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Temporal(TemporalType.DATE)
+    @Column(nullable = false)
+    private Date birthday;
+
+    @Temporal(TemporalType.DATE)
+    @Column(nullable = true)
+    private Date registrationDate;
+
+    @Column(nullable = true, length = 64)
+    private String photos;
+
     @ElementCollection(fetch = FetchType.EAGER)
     private List<Role> roles;
+
+    @Column(name = "verification_code",nullable = true)
+    private String verificationCode;
+
+    @Column(name = "enabled",nullable = true)
+    private boolean enabled;
+
 }
