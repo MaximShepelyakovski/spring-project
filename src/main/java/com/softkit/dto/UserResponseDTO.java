0ef5be84878808dc214fdb9529dff8db0a70eb24
Maxim Shepelyakvoski <maxim.shepelyakovski@yahoo.com>
2022-09-23 14:11:15 +0300

    adding new fields

--- src/main/java/com/softkit/dto/UserResponseDTO.java
+++ src/main/java/com/softkit/dto/UserResponseDTO.java
@@ -5,9 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
 import java.util.List;
 
 @Data
+
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserResponseDTO {
@@ -15,6 +17,11 @@ public class UserResponseDTO {
     private Integer id;
     private String username;
     private String email;
+    private String firstName;
+    private String lastName;
+    private Date birthday;
+    private Date registrationDate;
+    private String photos;
     private List<Role> roles;
 
 }
