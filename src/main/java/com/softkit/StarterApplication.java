d68ef7ede40daebe12618577e74f7892c550ebce
Maxim Shepelyakvoski <maxim.shepelyakovski@yahoo.com>
2022-09-23 14:06:22 +0300

    add @EnableCaching

--- src/main/java/com/softkit/StarterApplication.java
+++ src/main/java/com/softkit/StarterApplication.java
@@ -2,8 +2,10 @@ package com.softkit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class StarterApplication {
     
     public static void main(String[] args) {
