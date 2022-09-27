3f3f9831acfb7343cae0d860a313d809ca28d707
Maxim Shepelyakvoski <maxim.shepelyakovski@yahoo.com>
2022-09-27 13:26:23 +0300

    update and add new repository

--- src/main/java/com/softkit/repository/UserRepository.java
+++ src/main/java/com/softkit/repository/UserRepository.java
@@ -1,22 +1,18 @@
 package com.softkit.repository;
 
 import com.softkit.model.User;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-
     boolean existsByUsername(String username);
-
     boolean existsByUsernameIgnoreCase(String username);
-
     User findByUsername(String username);
-
     boolean existsByEmailIgnoreCase(String email);
-
     boolean existsByVerificationCode(String verificationCode);
-
     User findByVerificationCode(String verificationCode);
-
     Long countByEnabledIsTrue();
-
+    User findByEmail(String usernameOrEmail);
+    User save(User user);
 }
